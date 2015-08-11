6200a9c20dbe16024a9566e1563b75193bce6a25
RichardCSantana <richardcsantana@gmail.com>
2015-08-11 14:25:39 -0300

    adição do git para uso do bower

--- angular/machine.sh
+++ angular/machine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-add-repository ppa:chris-lea/node.js;
 sudo apt-get update;
-sudo apt-get install -y ruby-dev nodejs build-essential;
+sudo apt-get install -y ruby-dev nodejs build-essential git;
 sudo apt-get autoremove -y;
 sudo npm cache clean -f;
 sudo npm install -g npm;
@@ -10,4 +10,4 @@ n stable;
 sudo npm install -g yo;
 cd /home/user/vagrant;
 echo "export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules" >> ~/.bashrc && source ~/.bashrc;
-sudo npm install bower grunt-cli gulp compass generator-karma generator-angular;
+sudo npm install -g bower grunt-cli gulp compass generator-karma generator-angular;
