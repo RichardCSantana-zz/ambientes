d6dc6ee54212e52d9378a88d4298312ea01e920c
RichardCSantana <richardcsantana@gmail.com>
2015-08-14 01:34:54 -0300

    adição do spring-boot cli

--- spring-boot/config.sh
+++ spring-boot/config.sh
@@ -3,3 +3,8 @@ echo "export M2_HOME" >> ~/.bashrc
 echo "M2="$M2_HOME"/bin" >> ~/.bashrc
 echo "export M2" >> ~/.bashrc
 echo "export PATH="$PATH:$M2 >> ~/.bashrc && source ~/.bashrc
+
+curl -s get.gvmtool.net | bash
+source "/home/vagrant/.gvm/bin/gvm-init.sh"
+gvm install springboot
+. ~/.gvm/springboot/current/shell-completion/bash/spring
